0819ec17d955cd38fa39d747a3e9080437a20152
safwan-ms <safwan.anasms@gmail.com>
2025-08-02 19:35:30 +0530

    feat: Add types for message props

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
 import { assets } from "../../../assets/assets";
 
-const Message = ({ role, content }) => {
+interface MessageProps {
+  role: string;
+  content: string;
+}
+
+const Message = ({ role, content }: MessageProps) => {
   return (
     <div className="flex flex-col items-center w-full max-w-3xl text-sm">
       <div
